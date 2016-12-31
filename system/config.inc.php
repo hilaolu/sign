3911bc3d9d82f13aca7c02432b3045a8036ad17f
hilaolu <hilaolu@foxmail.com>
2016-12-31 22:45:55 +0800

    Update config.inc.php

--- system/config.inc.php
+++ system/config.inc.php
@@ -4,10 +4,10 @@ $_config = array (
   'version' => '1.14.6.2',
   'db' => 
   array (
-    'server' => OPENSHIFT_MYSQL_DB_HOST,
-    'port' => 3306,
-    'username' => OPENSHIFT_MYSQL_DB_USERNAME,
-    'password' => OPENSHIFT_MYSQL_DB_PASSWORD,
+    'server' => 'OPENSHIFT_MYSQL_DB_HOST',
+    'port' => '3306',
+    'username' => 'OPENSHIFT_MYSQL_DB_USERNAME',
+    'password' => 'OPENSHIFT_MYSQL_DB_PASSWORD',
     'name' => 'sign',
     'pconnect' => true,
   ),
