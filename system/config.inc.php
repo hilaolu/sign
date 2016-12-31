fd85f52d8efca165fceac3965e41493f322a7550
hilaolu <hilaolu@foxmail.com>
2016-12-31 22:19:36 +0800

    23333
    2333333333333333

---
+++ system/config.inc.php
@@ -0,0 +1,15 @@
+<?php
+/* Auto-generated config file */
+$_config = array (
+  'version' => '1.14.6.2',
+  'db' => 
+  array (
+    'server' => '127.7.204.130',
+    'port' => 3306,
+    'username' => 'adminyRTu5qJ',
+    'password' => 'Zp9jiUXnXHGe',
+    'name' => 'sign',
+    'pconnect' => true,
+  ),
+);
+?>
